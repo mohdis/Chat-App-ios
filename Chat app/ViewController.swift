a405a8ab7a3f2ac462e981e62e270ca92ced7799
Amir Mahdi Soleimani <Mohdis.solimani@gmail.com>
2019-10-27 12:14:04 +0330

    Initial Commit

---
+++ Chat app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat app
+//
+//  Created by Amir Mahdi Soleimani on 10/27/19.
+//  Copyright © 2019 Amir Mahdi Soleimani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
